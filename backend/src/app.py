0ca364f509f67f3037966b7c8493911fa9b7fb77
JosecoLoco <jose.pereztapia@gmail.com>
2025-04-09 14:01:15 -0500

    Recolocación de archivos

---
+++ backend/src/app.py
@@ -0,0 +1,78 @@
+from flask import Flask, jsonify, request
+from flask_pymongo import PyMongo
+from flask_cors import CORS
+from bson import ObjectId
+
+
+app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://localhost/flaskreactcrud'
+mongo = PyMongo(app)
+
+
+CORS(app)
+
+db = mongo.db.pythonreact
+
+
+@app.route('/users', methods=['POST'])
+def createUser():
+    print(request.json)
+    user = {
+        'name': request.json['name'],
+        'email': request.json['email'],
+        'password': request.json['password']
+    }
+    result = db.insert_one(user)  
+    return jsonify({'_id': str(result.inserted_id)})  
+
+
+@app.route('/users', methods=['GET'])
+def getUsers():
+    users = []
+    for doc in db.find():
+        users.append({
+            '_id': str(doc['_id']),  
+            'name': doc['name'],
+            'email': doc['email'],
+            'password': doc['password']
+        })
+    return jsonify(users)
+
+@app.route('/users/<id>', methods=['GET'])
+def getUser(id):
+    user = db.find_one({'_id': ObjectId(id)})
+    if user:  
+        return jsonify({
+            '_id': str(user['_id']),
+            'name': user['name'],
+            'email': user['email'],
+            'password': user['password']
+        })
+    else:
+        return jsonify({'error': 'User not found'}), 404  
+
+@app.route('/users/<id>', methods=['DELETE'])
+def deleteUser(id):
+    result = db.delete_one({'_id': ObjectId(id)})
+    if result.deleted_count > 0:
+        return jsonify({'message': 'User Deleted'})
+    else:
+        return jsonify({'error': 'User not found'}), 404
+
+@app.route('/users/<id>', methods=['PUT'])
+def updateUser(id):
+    print(request.json)
+    result = db.update_one({'_id': ObjectId(id)}, {"$set": {
+        'name': request.json['name'],
+        'email': request.json['email'],
+        'password': request.json['password']
+    }})
+    if result.matched_count > 0:
+        return jsonify({'message': 'User Updated'})
+    else:
+        return jsonify({'error': 'User not found'}), 404
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
